4236ebe87ce04e2e6e20b41aa11d9a8fc92d8cf7
elmer-ajio <elmer@ajio.co.jp>
2025-04-16 16:31:56 +0800

    reusable table component

--- client/src/components/_custom/CustomTable.tsx
+++ client/src/components/_custom/CustomTable.tsx
@@ -8,19 +8,23 @@ import {
   TableCell,
   TableBody,
   Paper,
+  Skeleton,
+  Box,
 } from "@/src/html";
 
-interface CustomTableProps {
-  tableHeader: string[];
-  tableData: string[];
+interface CustomTableProps<TData> {
+  tableHeader: (string | { label: string; key: keyof TData })[];
+  tableData: TData[];
   tableInfo?: string;
+  loading: boolean;
 }
 
-const CustomTable: React.FC<CustomTableProps> = ({
+const CustomTable = <TData extends Record<string, any>>({
   tableHeader,
   tableData,
   tableInfo,
-}) => {
+  loading,
+}: CustomTableProps<TData>) => {
   return (
     <TableContainer
       component={Paper}
@@ -30,21 +34,89 @@ const CustomTable: React.FC<CustomTableProps> = ({
         <TableHead>
           <TableRow>
             {tableHeader.map((tHeader, index) => (
-              <TableCell key={index}>{tHeader}</TableCell>
+              <TableCell
+                key={index}
+                sx={{
+                  textAlign: "center",
+                  verticalAlign: "middle",
+                }}
+              >
+                {typeof tHeader === "object" ? tHeader.label : tHeader}
+              </TableCell>
             ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          {tableData.length > 0 ? (
-            tableData.map((dBody, index) => (
-              <TableRow key={index}>
-                <TableCell>{dBody}</TableCell>
-              </TableRow>
-            ))
+          {loading ? (
+            <TableRow>
+              {tableHeader.map((_, index) => (
+                <TableCell
+                  key={index}
+                  sx={{
+                    textAlign: "center",
+                    verticalAlign: "middle",
+                  }}
+                >
+                  <Skeleton
+                    variant="text"
+                    sx={{
+                      maxWidth: "100%",
+                    }}
+                  />
+                </TableCell>
+              ))}
+            </TableRow>
+          ) : tableData.length > 0 ? (
+            <>
+              {tableData.map((dBody, rowIndex) => (
+                <TableRow key={rowIndex}>
+                  {tableHeader.map((header, colIndex) => (
+                    <TableCell
+                      key={colIndex}
+                      sx={{
+                        textAlign: "center",
+                        verticalAlign: "middle",
+                        ...(header === "Actions" && {
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }),
+                      }}
+                    >
+                      {typeof header === "object"
+                        ? dBody[header.key]
+                        : header === "Actions" &&
+                          dBody["actions"]?.map(
+                            (item: React.ReactNode, index: number) => (
+                              <Box key={index}>{item}</Box>
+                            ),
+                          )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </>
           ) : (
             <TableRow>
-              {" "}
-              <TableCell>{tableInfo}</TableCell>
+              <TableCell
+                colSpan={tableHeader.length}
+                sx={{
+                  textAlign: "center",
+                  verticalAlign: "middle",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                    height: "100px",
+                  }}
+                >
+                  {tableInfo}
+                </Box>
+              </TableCell>
             </TableRow>
           )}
         </TableBody>
